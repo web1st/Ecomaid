899bc3b62884788c7df55e8da2bb33a6792c974f
Jose Miguel Galvan <Miguelborquez7@gmail.com>
2023-01-18 16:54:13 -0800

    finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get 'help' => 'static_pages#help'
   get 'about' => 'static_pages#about'
   get 'contact' => 'static_pages#contact'
+  get '/signup' => 'users#new'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
