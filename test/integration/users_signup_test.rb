fc39c3ff4c8a2964a5405edc02a455b7197c2f2f
Jose Miguel Galvan <Miguelborquez7@gmail.com>
2023-04-13 13:16:40 -0700

    sign up no display errors

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -3,14 +3,17 @@
 require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
-
   test "invalid signup information" do
-    get signup_path
+    get new_user_path
     assert_no_difference 'User.count' do
-      post users_path params: { user: { name: "",
-                                         email: "user@invalid",
-                                         password:              "bar",
-                                         password_confirmation: "foobar" } }
+      post users_path params: {
+        user: { 
+          name: " ",
+          email: "user@invalid",
+          password:"bar",
+          password_confirmation: "foobar" 
+          }
+         }
   end
   assert_template 'users/new'
   assert_select 'div#error_explanation'
